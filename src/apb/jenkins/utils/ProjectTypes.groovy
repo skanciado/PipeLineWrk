ebc4537e3f44a223a33a05d19bcca6a14d4bef76
Daniel Horta <skanciado@gmail.com>
2022-06-21 11:26:19 +0200

    aad

--- src/apb/jenkins/utils/ProjectTypes.groovy
+++ src/apb/jenkins/utils/ProjectTypes.groovy
@@ -4,5 +4,13 @@ public enum ProjectTypes {
     MAVEN ("maven"),
     DOTNET("dotnet"),
     DOCKER("docker")
-     
+    private final String name;
+
+    private ProjectTypes(String s) {
+        name = s;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
